0702a90f410cdb7590804bf12d9a11e943d93cb3
Melissa Astbury <melissajastbury@gmail.com>
2020-04-12 16:57:46 +0100

    added the header component and this is visable on each page

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Switch, Route } from "react-router-dom";
 import "./App.scss";
 import Homepage from "./pages/Homepage/homepage";
 import ShopPage from "./pages/Shop/shop";
+import Header from "./components/Header/header";
 
 function App() {
   return (
     <>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/shop" component={ShopPage} />
---
+++ src/components/Header/header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { ReactComponent as Logo } from "../../assets/images/crown.svg";
+import "./header.scss";
+
+const header = () => {
+  return (
+    <div className="header">
+      <Link className="logo-container" to="/">
+        <Logo className="logo" />
+      </Link>
+      <div className="options">
+        <Link className="option" to="/shop">
+          SHOP
+        </Link>
+        <Link className="option" to="/shop">
+          CONTACT
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default header;
