4518a5679009f6b6f497d1d1494cc8aa55375518
Melissa Astbury <melissajastbury@gmail.com>
2020-04-14 10:49:32 +0100

    added a user.reducer and root-reducer file and written the code for my first reducer

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import "./index.scss";
 import App from "./App";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
+  <Provider>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
