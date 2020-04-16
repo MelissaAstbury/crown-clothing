91d082f082d8b53fc4904cfbe3be8a2283417a00
Melissa Astbury <melissajastbury@gmail.com>
2020-04-16 16:02:04 +0100

    adding persist

--- src/components/CartDropDown/cart-dropdown.jsx
+++ src/components/CartDropDown/cart-dropdown.jsx
@@ -15,14 +15,15 @@ import "./cart-dropdown.scss";
 
 const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown">
-    <div className="cart-items" />
-    {cartItems.length ? (
-      cartItems.map((cartItem) => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      ))
-    ) : (
-      <span className="empty-message">Your cart is empty</span>
-    )}
+    <div className="cart-items">
+      {cartItems.length ? (
+        cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
+      ) : (
+        <span className="empty-message">Your cart is empty</span>
+      )}
+    </div>
     <CustomButton
       onClick={() => {
         history.push("/checkout");
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 
 import "./index.scss";
 import App from "./App";
@@ -11,9 +12,11 @@ import App from "./App";
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+      <PersistGate persistor={persistor}>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
