40cec0b11e0c439c2a17ae992050b24387173317
Melissa Astbury <melissajastbury@gmail.com>
2020-04-17 16:02:20 +0100

    fixed the advanced Routeing so the .../hats page shows the items

--- src/pages/Collection/collection.jsx
+++ src/pages/Collection/collection.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import CollectionItem from "../../components/CollectionItem/collection-item";
 
+import { selectCollection } from "../../redux/Shop/shop.selectors";
+
 import "./collection.scss";
 
-const CollectionPage = ({ match }) => (
-  <div className="collection-page">
-    <h2>COLLECTION PAGE</h2>
-  </div>
-);
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
--- src/pages/Shop/shop.jsx
+++ src/pages/Shop/shop.jsx
@@ -1,14 +1,12 @@
-// import React, {Component} from 'react';
 import React from "react";
 import { Route } from "react-router-dom";
 
-import CollectionOverview from "../../components/CollectionsOverview/collections-overview";
+import CollectionsOverview from "../../components/CollectionsOverview/collections-overview";
 import CollectionPage from "../Collection/collection";
 
-// class ShopPage extends Component
 const ShopPage = ({ match }) => (
   <div className="shop-page">
-    <Route exact path={`${match.path}`} component={CollectionOverview} />
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
     <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
