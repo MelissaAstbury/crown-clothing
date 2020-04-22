a35143fe932099c47616add5f6de7b183319db61
Melissa Astbury <melissajastbury@gmail.com>
2020-04-22 10:44:49 +0100

    added styled components to my custome-button component

--- src/components/CustomButton/custom-button.jsx
+++ src/components/CustomButton/custom-button.jsx
@@ -1,23 +1,11 @@
 import React from "react";
 
-import "./custom-button.scss";
+// import "./custom-button.scss";
 
-const CustomButton = ({
-  children,
-  isGoogleSignIn,
-  inverted,
-  ...otherProps
-}) => {
-  return (
-    <button
-      className={`${inverted ? "inverted" : ""} ${
-        isGoogleSignIn ? "google-sign-in" : ""
-      } custom-button`}
-      {...otherProps}
-    >
-      {children}
-    </button>
-  );
-};
+import { CustomButtonContainer } from "./custom-button.styles";
+
+const CustomButton = ({ children, ...props }) => (
+  <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
+);
 
 export default CustomButton;
